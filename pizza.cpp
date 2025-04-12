f4cc029006cb9c5d8b428b5d0672096a661241ec
gabriel <gabrielkr32@gmail.com>
2025-04-11 21:36:02 -0300

    projetoPizza_Init

---
+++ pizza.cpp
@@ -0,0 +1,31 @@
+// sistema de cardapio de Pizza
+
+//projeto simples que caluclara o custo total da ordem. 
+
+
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PIZZA = 15.50,
+                 SODA = 2.00,
+                 CHICKEN_NUGGETS = 7.00,
+                 BREADSTICKS = 9.75;
+    double numItens = 0,
+           menuItem,
+           total = 0;
+    char itemLetter;
+    
+    cout << "=========================================\n"
+         <<"\t\tWalcon to Pizza palace\n"
+         << "===========================================" << endl;
+    cout << fixed << setprecision(2) << right;  
+    
+    do{
+        cout <<"===================================================\n\n"
+             <<
+    }
+
+}
