00cfef9e17fa67e6e2a31e8130cfdb88b7704da6
gabriel <gabrielkr32@gmail.com>
2025-04-14 19:47:12 -0300

    Corrigindo erro da condiçao

--- pizza.cpp
+++ pizza.cpp
@@ -25,7 +25,48 @@ int main() {
     
     do{
         cout <<"===================================================\n\n"
-             <<
-    }
+             << "\tA\tPizza" << setw(26) << PIZZA
+             << "\n\tB\tB\tSoda" <<setw(27) << SODA
+             << "\n\tC\tChicken Nuggets" << setw(20) << CHICKEN_NUGGETS
+             << "\n\tD\tBreadstickes" << setw(20) << BREADSTICKS
+             << "\n\tE\tExit" << endl;
 
+
+        cout << "\nPlease enter the next menu item Letter: ";
+        cin >> itemLetter;
+        
+        
+        switch(itemLetter){
+            case 'A':
+            menuItem = PIZZA;
+            break;
+            case 'B':
+            menuItem = SODA;
+            break;
+            case 'C':
+            menuItem = CHICKEN_NUGGETS;
+            break;
+            case 'D':
+            menuItem = BREADSTICKS;
+            break;
+            case 'E':
+            break;
+            default:
+                cerr << "\nPlease pick a valid option\n";
+        }
+    if(itemLetter == 'A' || itemLetter == 'B' || itemLetter == 'C' || itemLetter == 'D')
+       numItens++;
+
+    cout << "\nNumber of itens: " << "\nNumber of itens: " << setprecision(0) << numItens << endl;
+
+    if (itemLetter == 'A' || itemLetter == 'B' || itemLetter == 'C' || itemLetter == 'D')
+    total += menuItem;
+
+    cout << "Total: " << setprecision(2) << total << endl;
+
+    } while (itemLetter != 'E');
+
+    cout <<"\nThank you Enjoy!" <<  endl;
+
+    return 0;
 }
