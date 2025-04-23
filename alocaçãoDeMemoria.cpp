38b7f5a435274f6e4bb56dde70f047cf4bfb99ba
gabriel <gabrielkr32@gmail.com>
2025-04-23 18:32:55 -0300

    Projeto iniciado/finalizado

--- alocaçãoDeMemoria.cpp
+++ alocaçãoDeMemoria.cpp
@@ -1,1 +1,20 @@
-//
+//projeto para contar e mostrar dados e indo de uma partidada de jogo
+
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+void input (string nems[], int marcehs [], int size);
+void display(const string names[], const int mathces[], int size);
+void displayMostMaches(const string names[], const int matches[], int size);
+double mean(const int matches[], int num);
+void sortStudentes(string names[], int matches[], int size);
+
+
+
+
